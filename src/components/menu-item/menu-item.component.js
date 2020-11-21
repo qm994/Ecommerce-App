d4de277051ea4e3452c5ff2a3b5aff913d28ef4a
Qingyuan Ma <qingyuan@Justins-MacBook-Pro-166.local>
2020-11-21 15:19:58 -0500

    split the homepage components + 1

--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './menu-item.styles.scss';
 
-const MenuItem = ( {title} ) => (
+const MenuItem = ( {title, id, imageUrl} ) => (
     <div class="menu-item">
         <div class="content">
             <h1 class="title">{title}</h1>
