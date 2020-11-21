75bb344723f2464f6d02e2d89489ce064f41993d
Qingyuan Ma <qingyuan@Justins-MacBook-Pro-166.local>
2020-11-20 22:38:14 -0500

    init app with test component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import HomePage from './homepage.component';
 
 function App() {
   return (
     <div>
-      <h1>hellp firy</h1>
+      <HomePage />
     </div>
   );
 }
---
+++ src/homepage.component.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import './homepage.styles.scss';
+const HomePage = () => (
+    <div class="homepage">
+        <div class="directory-menu">
+            <div class="menu-item">
+                <div class="content">
+                    <h1 class="title">hats</h1>
+                    <span class="subtitle">shop now</span>
+                </div>
+            </div>
+
+            <div class="menu-item">
+                <div class="content">
+                    <h1 class="title">jacket</h1>
+                    <span class="subtitle">shop now</span>
+                </div>
+            </div>
+
+            <div class="menu-item">
+                <div class="content">
+                    <h1 class="title">snakers</h1>
+                    <span class="subtitle">shop now</span>
+                </div>
+            </div>
+
+            <div class="menu-item">
+                <div class="content">
+                    <h1 class="title">mans</h1>
+                    <span class="subtitle">shop now</span>
+                </div>
+            </div>
+
+            <div class="menu-item">
+                <div class="content">
+                    <h1 class="title">women</h1>
+                    <span class="subtitle">shop now</span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default HomePage;
