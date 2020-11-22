f849ba45957b7655e75a5bfea00e0089ea985182
Qingyuan Ma <qingyuan@Justins-MacBook-Pro-166.local>
2020-11-22 11:04:03 -0500

    add hover and finish style for menu-item

--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -2,13 +2,11 @@ import React from 'react';
 import './menu-item.styles.scss';
 
 const MenuItem = ( {title, id, imageUrl, size} ) => (
-    <div 
-        className={`${size} menu-item`}
-        style={{ backgroundImage: `url(${imageUrl})` }}
-        >
-        <div class="content">
-            <h1 class="title">{title}</h1>
-            <span class="subtitle">shop now</span>
+    <div className={`${size} menu-item`}>
+        <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }} />
+        <div className="content">
+            <h1 className="title">{title.toUpperCase()}</h1>
+            <span className="subtitle">shop now</span>
         </div>
     </div>
 
