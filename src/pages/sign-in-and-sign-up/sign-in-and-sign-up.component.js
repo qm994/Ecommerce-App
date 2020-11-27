de79bced671754c60c144a6f4ba9fd500860d99c
Qingyuan Ma <qingyuan@Justins-MacBook-Pro-166.local>
2020-11-26 19:56:51 -0500

    complete the sign up with email and pass

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.js
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './sign-in-and-sign-up.styles.scss';
 
-import SignIn from '../../components/sign-in/sign-in.component'
-
+import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 const SignInSignUpPage = () => {
 
     return (
-        <div class="sign-in-and-sign-up"><SignIn /></div>
+        <div class="sign-in-and-sign-up">
+            <SignIn />
+            <SignUp />
+        </div>
     )
 }
 
