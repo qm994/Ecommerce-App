0142f2a6aeca241eab379de95adbe44044a733fe
Qingyuan Ma <qingyuan@Justins-MacBook-Pro-166.local>
2020-11-26 21:09:36 -0500

    type to google sign in button

--- src/components/sign-in/sign-in.component.js
+++ src/components/sign-in/sign-in.component.js
@@ -62,7 +62,7 @@ class SignIn extends React.Component {
                         handleChange={this.handleChange} required />
                     <div class="buttons">
                         <CustomButton type='submit'>Sign In</CustomButton> 
-                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn> 
+                        <CustomButton type='button' onClick={signInWithGoogle} isGoogleSignIn> 
                             {''}
                             Sign In with Google{''}
                         </CustomButton>
