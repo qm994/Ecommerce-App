add1425bed59d773adfe68582f2c9be2a9b20b3a
Qingyuan Ma <qingyuan@Justins-MacBook-Pro-166.local>
2020-11-28 12:59:53 -0500

    modify custom button and add to collections

--- src/components/collection-item/collection-item.component.js
+++ src/components/collection-item/collection-item.component.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './collection-item.styles.scss';
-
+import CustomButton from '../custom-button/custom-button.component'; 
 const CollectionItem = ({ id, name, price, imageUrl }) => {
     return (
         <div class="collection-item">
@@ -15,8 +15,10 @@ const CollectionItem = ({ id, name, price, imageUrl }) => {
             <div class="collection-footer">
                 <span class="name">{name}</span>
                 <span class="price">{price}</span>
-                
             </div>
+            <CustomButton inverted >
+                Add to cart
+            </CustomButton>
         </div>
     )
 }
