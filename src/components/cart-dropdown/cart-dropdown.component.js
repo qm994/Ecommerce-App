d01ea312d36918761626ace5fc882796bd2518b7
Qingyuan Ma <qingyuan@Justins-MacBook-Pro-166.local>
2020-11-29 11:37:19 -0500

    use memoized selector for cart dropdown and cart icon count

--- src/components/cart-dropdown/cart-dropdown.component.js
+++ src/components/cart-dropdown/cart-dropdown.component.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import CustomButton from '../../components/custom-button/custom-button.component';
 import './cart-dropdown.styles.scss';
 import CartItem from '../cart-item/cart-item.component';
-
+import { selectCartItems } from '../../redux/cart/cart.selectors';
 
 const CartDropdown = ({cartItems}) => {
     return (
@@ -21,7 +21,7 @@ const CartDropdown = ({cartItems}) => {
 }
 
 const mapStateToProps = (state) => ({ //or we can deconstruct { cart: { cartItems } }
-    cartItems: state.cart.cartItems
+    cartItems: selectCartItems(state)
 })
 
 export default connect(mapStateToProps, null)(CartDropdown);
--- src/components/cart-icon/cart-icon.component.js
+++ src/components/cart-icon/cart-icon.component.js
@@ -3,7 +3,7 @@ import './cart-icon.styles.scss';
 import { connect } from 'react-redux';
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
-
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 const CartIcon = ({ toggleCartHidden, itemCount }) => {
@@ -19,11 +19,10 @@ const mapDispatchToProps = (dispatch) => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
-const mapStateToProps = ({ cart: {cartItems} }) => ({
-    itemCount: cartItems.reduce(
-        (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity,
-        0
-    )
-})
+const mapStateToProps = (state) => {
+    {console.log('I am invoked!')}
+    return ({
+        itemCount: selectCartItemsCount(state)
+})}
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
