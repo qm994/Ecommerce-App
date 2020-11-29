f1cd3606209a56da69c646d0986419a498deb902
Qingyuan Ma <qingyuan@Justins-MacBook-Pro-166.local>
2020-11-29 15:22:02 -0500

    add redux persist for cartItems

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,22 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+// storage is local storage here
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
+
+const persistConfig = {
+    'key': 'root',
+    storage,
+    // the reducer to persist
+    whitelist: ['cart']
+}
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
 })
+
+export default persistReducer(persistConfig, rootReducer)
