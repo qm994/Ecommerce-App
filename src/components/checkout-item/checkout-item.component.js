5836a0c44748f4d467eaddd9f82e6514ee4c5f31
Qingyuan Ma <qingyuan@Justins-MacBook-Pro-166.local>
2020-11-29 14:11:44 -0500

    add remove cart items action, reducer and connect with checkout item component

--- src/components/checkout-item/checkout-item.component.js
+++ src/components/checkout-item/checkout-item.component.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './checkout-item.styles.scss';
+import { connect } from 'react-redux';
 
+import { clearItemFromCart } from '../../redux/cart/cart.actions';
 
-const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => {
-
+const CheckoutItem = ({ cartItem, clearItem }) => {
+    const {name, imageUrl, price, quantity} = cartItem
     return (
         <div class="checkout-item">
             <div class="image-container">
@@ -12,9 +14,13 @@ const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => {
             <span class="name">{name}</span>
             <span class="quantity">{quantity}</span>
             <span class="price">{price}</span>
-            <div class="remove-button">&#10005;</div>
+            <div class="remove-button" onClick={() => clearItem(cartItem)}>&#10005;</div>
         </div>
     )
 }
 
-export default CheckoutItem;
+const mapDispatchToProps = (dispatch) => ({
+    clearItem: item => dispatch(clearItemFromCart(item))
+})
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
