1aa3fed5d2614b891db920ba5e60115231d237ac
Qingyuan Ma <qingyuan@Justins-MacBook-Pro-166.local>
2020-11-30 10:25:03 -0500

    add source map for better debug

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -3,17 +3,14 @@ import { createSelector } from 'reselect';
 
 const selectShop = state => state.shop;
 
-const COLLECTION_ID_MAP = {
-    hats: 1,
-    sneakers: 2,
-    jackets: 3,
-    womens: 4,
-    mens: 5
-}
-
 export const selectCollections = createSelector(
     selectShop,
     shop => shop.collections
+)
+
+export const selectCollectionsForPreview = createSelector(
+    selectCollections,
+    collections => Object.keys(collections).map(key => collections[key])
 )
 
 // it will export the collection match the url id  from collections
