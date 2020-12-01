221d69a4d5575105e3d3f2a0baed2d09f17a87b4
Qingyuan Ma <qingyuan@Justins-MacBook-Pro-166.local>
2020-12-01 18:59:00 -0500

    add requeste depend and init test with shop reducer

---
+++ src/App.test.ts
@@ -0,0 +1,6 @@
+import React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'
+import App from './App';
+import { selectCurrentUser } from './redux/user/user.selectors';
+
